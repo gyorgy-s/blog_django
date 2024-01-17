935c0c42ec38ac10e1f0cd35841c9a0d892b9de7
gyorgy <none>
2024-01-17 03:28:16 +0100

    feat: add basic templates with styling

---
+++ my_site/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", view=views.home, name="home"),
+    path("posts", view=views.posts, name="posts"),
+    path("posts/<slug:slug>", view=views.post, name="post"),  # /posts/my-first-post
+]
